4701e93e057f2a0034813f97bb17aee11b5e9242
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-12-09 12:55:42 +0100

    second header end

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="header">
-      Qualcosa
+      <div class="container">
+          <div class="brand">
+              <img src="@/assets/classic-shop/images/classic_shop_logo2x.png" alt="">
+          </div>
+
+          <div class="button-selection">
+                <div class = "selection">
+                    Home
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class = "selection">
+                    Shop
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class = "selection">
+                    Products
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class = "selection">
+                    Categories
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class = "selection">
+                    News
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class = "selection">
+                    Elements
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+
+                <div class="shop-now">
+                    <strong>SHOP NOW!</strong>
+                </div>
+
+                <div class="search">
+                    <i class="fas fa-search"></i>
+                </div>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -11,4 +50,55 @@ export default {
 </script>
 
 <style scoped lang = "scss">
+.header {
+    height: 100px;
+    .container {
+        width: 1200px;
+        height: 100%;
+        margin: auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .brand {
+            height: 100%;
+            display: flex;
+            align-items: center;
+            img {
+                width: 60%;
+            }
+        }
+        .button-selection {
+            display: flex;
+            align-items: center;
+            height: 100%;
+            .selection {
+                padding: 0 20px;
+                font-size: 15px;
+                color: gray;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                border-top: 4px solid white;
+                &:hover {
+                border-top: 4px solid #427ed5;
+                color: #427ed5;
+                }
+                i {
+                    padding: 0 0 0 5px;
+                    font-size: 10px;
+                }
+            }
+            .shop-now {
+                color: white;
+                background-color: #427ed5;
+                padding: 15px 30px;
+                border-radius: 25px;
+                font-size: 13px;
+            }
+            .search {
+                padding: 0 0 0 30px;
+            }
+        }
+    }
+}
 </style>
