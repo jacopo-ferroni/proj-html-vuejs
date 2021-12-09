fdfb221a49b7aad92386ddfa316e98abac50bc57
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-12-09 10:57:59 +0100

    start

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+      Qualcosa
+  </div>
+</template>
+
+<script>
+export default {
+    name : `Footer`,
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+      Qualcosa
+  </div>
+</template>
+
+<script>
+export default {
+    name : `Main`,
+}
+</script>
+
+<style scoped lang = "scss">
+
+</style>
