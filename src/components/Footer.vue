d75b13c64997ed419b500fe627ca15a9b314da69
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-12-11 10:24:14 +0100

    footer end

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
 
         </div>
         <div class="top-rated">
-            <span>TOP RATED</span>
+            <span class="toprated-title">TOP RATED PRODUCTS</span>
                 <div class="feed">
                     <div class="feed-text">
                         <span>Leather Gloves</span>
@@ -84,10 +84,68 @@
                 <hr size = "1">
         </div>
         <div class="recent-post">
-
+          <div class="title-recent">
+            RECENT POSTS
+          </div>
+          <div class="line">
+            <i class="fas fa-chevron-right"></i>
+            <div>Aenean lobortis sapien enim viverra</div>
+          </div>
+          <hr size="1">
+          <div class="line">
+            <i class="fas fa-chevron-right"></i>
+            <div>Duis ac massa semper maximus</div>
+          </div>
+          <hr size="1">
+          <div class="line">
+            <i class="fas fa-chevron-right"></i>
+            <div>Nunc fermit nulla eu justo sem id</div>
+          </div>
+          <hr size="1">
+          <div class="line">
+            <i class="fas fa-chevron-right"></i>
+            <div>Aliquam posuere magna eget nibh</div>
+          </div>
+          <hr size="1">
+          <div class="line">
+            <i class="fas fa-chevron-right"></i>
+            <div>Cras ac nulla ac consecte rutrum</div>
+          </div>
+          <hr size="1">
+          <div class="line">
+            <i class="fas fa-chevron-right"></i>
+            <div>Fusce mattis nunc ut aliquam</div>
+          </div>
+          <hr size="1">
         </div>
         <div class="tags">
-
+          <div class="title-tag">
+            TAGS
+          </div>
+          <div class="tags-container">
+            <div class="tag">Black</div>
+            <div class="tag">boots</div>
+            <div class="tag">Brown</div>
+            <div class="tag">Casual</div>
+            <div class="tag">D&G</div>
+            <div class="tag">Fabric</div>
+            <div class="tag">flowers</div>
+            <div class="tag">Grey</div>
+            <div class="tag">Hat</div>
+            <div class="tag">Hipster</div>
+            <div class="tag">lines</div>
+            <div class="tag">multi-purpose</div>
+            <div class="tag">New York</div>
+            <div class="tag">Outdoors</div>
+            <div class="tag">red</div>
+            <div class="tag">responsive</div>
+            <div class="tag">summer</div>
+            <div class="tag">sweater</div>
+            <div class="tag">Travel</div>
+            <div class="tag">Warm</div>
+            <div class="tag">White</div>
+            <div class="tag">winter</div>
+          </div>
         </div>
     </div>
       
@@ -109,6 +167,7 @@ export default {
       display: flex;
       padding: 50px 0;
       .reference {
+        margin: 10px;
         width: 25%;
         display: flex;
         flex-direction: column;
@@ -131,11 +190,15 @@ export default {
         }
       }
       .top-rated {
+        margin: 10px;
         color: #dbdcdc;
         width: 25%;
         display: flex;
         flex-direction: column;
         align-items: flex-start;
+        .toprated-title {
+          margin-bottom: 30px;
+        }
         .feed {
           margin: 10px 0;
           width: 100%;
@@ -170,16 +233,50 @@ export default {
         }
       }
       .recent-post {
+        margin: 10px;
         width: 25%;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
+        .title-recent {
+          color: #dbdcdc;
+          margin-bottom: 30px;
+        }
+        .line {
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+          color: #dbdcdc;
+          cursor: pointer;
+          i {
+            margin-right: 10px;
+            font-size: 13px;
+          }
+        }
       }
       .tags {
+        margin: 10px;
         width: 25%;
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        .title-tag {
+          color:#dbdcdc;
+          margin-bottom: 30px;
+        }
+          .tags-container {
+            display: flex;
+            flex-wrap: wrap;
+            .tag {
+              border: 1px solid rgba($color: #dbdcdc, $alpha: .1);
+              padding: 5px 10px;
+              color: #dbdcdc;
+              margin: 2px;
+              cursor: pointer;
+              &:hover {
+                border: 1px solid rgba($color: #dbdcdc, $alpha: .5);
+              }
+            }
+          }
       }
     }
   }
