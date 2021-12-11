1af45fd24c3a959bd5b918f7e11574d83e72f7d5
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-12-11 10:31:32 +0100

    end static part

--- src/components/Footer2.vue
+++ src/components/Footer2.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="footer2">
-        qualcosaaaa
+        <div class="footer-text">
+            ©Copyright 2012 - 2021 | Avada Theme by Theme Fusion | All Rights Reserved | Powered By JacopoFerroniPress
+        </div>
+        <img src="@/assets/classic-shop/images/payment_cards_footer.png" alt="">
     </div>
 </template>
 
@@ -10,6 +13,17 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+    .footer2 {
+        padding: 50px 0;
+        background-color: #1d1f22;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        .footer-text {
+            margin-bottom: 20px;
+            color: #dbdcdc;
+        }
+    }
 </style>
